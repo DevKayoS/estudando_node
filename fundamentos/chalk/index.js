0c09b69e084981303569407b311c0170a252dd26
DevKayoS <kayoribeirodev@gmail.com>
2024-04-03 14:13:16 -0300

    feat: aprendi a como melhorar a visualização do console usando o chalk

---
+++ fundamentos/chalk/index.js
@@ -0,0 +1,9 @@
+const chalk = require('chalk')
+
+const nota = 6
+
+if (nota >= 7){
+  console.log(chalk.green('Parabéns! Vocês está aprovado'))
+} else {
+  console.log(chalk.bgRed('Parabéns! Vocês está aprovado'))
+}
