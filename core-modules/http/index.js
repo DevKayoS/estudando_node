de4d3478ecd9198dda543c4092e421443d089c23
DevKayoS <kayoribeirodev@gmail.com>
2024-04-08 11:54:10 -0300

    feat: aprendendo como iniciar um servidor http somente com node

---
+++ core-modules/http/index.js
@@ -0,0 +1,13 @@
+const http = require("http")
+
+const port = 3000
+
+const server =  http.createServer((req, res) => {
+  res.write('Oi http')
+  res.end()
+
+})
+
+server.listen(port, ()=>{
+  console.log(`Server running in ${port}`)
+})
