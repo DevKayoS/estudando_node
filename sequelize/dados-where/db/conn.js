af79ee3e9f060e96067fe1c37f8b48389a9b869a
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-04-30 14:29:20 -0300

    feat: aprendendo a usar o where no sequelize

---
+++ sequelize/dados-where/db/conn.js
@@ -0,0 +1,20 @@
+//importando
+const {Sequelize} = require('sequelize')
+//fazendo a conexão e passando os dados necessários para isso
+const sequelize = new Sequelize('nodesequelize', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql'
+})
+
+// //fazendo a autentificação para realizar a conexão
+// try {
+
+//   sequelize.authenticate()
+//   console.log('conectamos com sucsso com o sequelize')
+  
+// } catch (error) {
+//   //imprimindo uma mensagem de erro caso dê algo errado
+//   console.log(`Não foi possível conectar: `, error)
+// }
+//exportando a configuração
+module.exports = sequelize
