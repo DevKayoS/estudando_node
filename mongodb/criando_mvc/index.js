336c8f655da28e1fd5d948ed2bf10a057bb9926a
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-05-29 13:08:00 -0300

    feat: fazendo uma estrutura mvc para usar com o mongodb

---
+++ mongodb/criando_mvc/index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const expbhs = require('express-handlebars')
+
+const app = express()
+
+const conn = require('./db/conn')
+
+const productsRoutes = require('./routes/productsRoutes')
+
+app.engine('handlebars', expbhs.engine())
+app.set('view engine', 'handlebars')
+
+app.use(
+  express.urlencoded({
+    extended:true
+  })
+)
+
+app.use(express.json())
+
+app.use(express.static('public'))
+
+app.use('/products', productsRoutes)
+
+app.listen(3333)
