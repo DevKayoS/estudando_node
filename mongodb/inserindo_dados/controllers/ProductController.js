cd5db800c3392d0c87e9cac4a0faaa2870d2faae
Kayo Vinicius <kayoribeirodev@gmail.com>
2024-05-29 13:53:49 -0300

    feat: aprendendo a inserir dados no mongdodb

---
+++ mongodb/inserindo_dados/controllers/ProductController.js
@@ -0,0 +1,21 @@
+const Product = require('../models/Product')
+
+module.exports  = class ProductController {
+  static showProducts(req, res) {
+    res.render('products/all')
+  }
+  static createProduct(req,res){
+    res.render('products/create')
+  }
+  static async saveProduct(req,res){
+    const name =  req.body.name
+    const price = req.body.price
+    const description =  req.body.description
+    
+    const product = new Product(name, price, description)
+
+    product.save()
+
+    res.redirect('/products')
+  }
+}
---
+++ mongodb/inserindo_dados/routes/productsRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+const ProductController = require('../controllers/ProductController')
+
+router.get('/create', ProductController.createProduct)
+router.post('/create', ProductController.saveProduct)
+router.get('/', ProductController.showProducts)
+
+module.exports = router
